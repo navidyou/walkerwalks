983ac42ea0390e1bc36a1cdb32134b0abe5f9991
navidyou <33015633+navidyou@users.noreply.github.com>
2022-02-13 15:00:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def main():
 #  viewer.launch(env, policy=random_policy)
   return 'Hello, World!'
 
+
 #env = suite.load(domain_name="humanoid", task_name="stand")
 #action_spec = env.action_spec()
 
@@ -33,8 +34,14 @@ def main():
   # Launch the viewer application.
 #  viewer.launch(env, policy=random_policy)
 
+#if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+#    app.run(host='127.0.0.1', port=8080, debug=True)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 #if __name__ == '__main__':
