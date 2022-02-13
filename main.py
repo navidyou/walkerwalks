04c9140fe830ac573872581dfd04f700df2d14a1
navidyou <33015633+navidyou@users.noreply.github.com>
2022-02-13 14:50:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,9 @@ def main():
   # Launch the viewer application.
 #  viewer.launch(env, policy=random_policy)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
 
 #if __name__ == '__main__':
 #  app.run(main)
