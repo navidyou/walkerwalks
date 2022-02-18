9cbe28340e2a2505806e522ebdb7c76ed2976761
navidyou <33015633+navidyou@users.noreply.github.com>
2022-02-17 21:47:15 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from dm_control import suite
-from dm_control import viewer
+#from dm_control import suite
+#from dm_control import viewer
 import numpy as np
 
 #from absl import app
 from flask import Flask
 
-env = suite.load(domain_name="humanoid", task_name="stand")
-action_spec = env.action_spec()
+#env = suite.load(domain_name="humanoid", task_name="stand")
+#action_spec = env.action_spec()
 
 # Define a uniform random policy.
-def random_policy(time_step):
-  del time_step  # Unused.
-  return np.random.uniform(low=action_spec.minimum,
-                           high=action_spec.maximum,
-                           size=action_spec.shape)
+#def random_policy(time_step):
+#  del time_step  # Unused.
+#  return np.random.uniform(low=action_spec.minimum,
+#                           high=action_spec.maximum,
+#                           size=action_spec.shape)
 
 app = Flask(__name__)
 
